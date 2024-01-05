e6e8b643aa0fa34dbeb46a11ee287de97742c4bf
JamMerRy <leejungkok556677@gmail.com>
2024-01-05 19:27:12 +0700

    feat: completed.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 
 
 # input
+name = input("enter your name : ")
 
 
 # print
+print("Hi", name, "I heard you're new to this  If you're not", name, "I'll teach you.")
+
+
+# Hi , ชื่อ , I heard you're new to this. If you're not ชื่อ , I'll teach you.
